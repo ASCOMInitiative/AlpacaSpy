9a45eb0b2d238b189cf3982b9e1ce5f72789be18
Peter Simpson <38855929+Peter-Simpson@users.noreply.github.com>
2026-07-11 12:19:26 +0100

    Improve playback reporting

---
+++ AlpacaSpy/Mismatch.cs
@@ -0,0 +1,16 @@
+﻿namespace AlpacaSpy
+{
+    /// <summary>
+    /// Represents a mismatch between the expected and actual data in the AlpacaSpy application.
+    /// </summary>
+    public class Mismatch(DateTime mismatchTime, string sendLogMessage, string responseLogMessage, string originalJsonString, string currentJsonString, string mismatchMessage, string propertyName)
+    {
+        public DateTime MismatchTime { get; set; } = mismatchTime;
+        public string SendLogMessage { get; set; } = sendLogMessage;
+        public string ResponseLogMessage { get; set; } = responseLogMessage;
+        public string OriginalJsonString { get; set; } = originalJsonString;
+        public string CurrentJsonString { get; set; } = currentJsonString;
+        public string MismatchMessage { get; set; } = mismatchMessage;
+        public string PropertyName { get; set; } = propertyName;
+    }
+}
