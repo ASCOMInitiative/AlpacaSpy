58083e6b651031e9d669e4486d39132eb0830450
Peter Simpson <38855929+Peter-Simpson@users.noreply.github.com>
2026-07-11 14:37:26 +0100

    Safety commit.

--- AlpacaSpy/Models/Mismatch.cs
+++ AlpacaSpy/Models/Mismatch.cs
@@ -1,16 +1,12 @@
-﻿namespace AlpacaSpy.Models
+﻿using System.Text.Json;
+
+namespace AlpacaSpy.Models
 {
-    /// <summary>
-    /// Represents a mismatch between the expected and actual data in the AlpacaSpy application.
-    /// </summary>
-    public class Mismatch(DateTime mismatchTime, string sendLogMessage, string responseLogMessage, string originalJsonString, string currentJsonString, string mismatchMessage, string propertyName)
+    public class Mismatch(string propertyName, string mismatchMessage, string originalValue, string currentValue)
     {
-        public DateTime MismatchTime { get; set; } = mismatchTime;
-        public string SendLogMessage { get; set; } = sendLogMessage;
-        public string ResponseLogMessage { get; set; } = responseLogMessage;
-        public string OriginalJsonString { get; set; } = originalJsonString;
-        public string CurrentJsonString { get; set; } = currentJsonString;
-        public string MismatchMessage { get; set; } = mismatchMessage;
         public string PropertyName { get; set; } = propertyName;
+        public string MismatchMessage { get; set; } = mismatchMessage;
+        public string OriginalValue { get; set; } = originalValue;
+        public string CurrentValue { get; set; } = currentValue;
     }
 }
---
+++ AlpacaSpy/Models/MismatchSummary.cs
@@ -0,0 +1,13 @@
+﻿namespace AlpacaSpy.Models
+{
+    /// <summary>
+    /// Represents a mismatch between the expected and actual data in the AlpacaSpy application.
+    /// </summary>
+    public class MismatchSummary(DateTime mismatchTime, string sendLogMessage, string responseLogMessage, List<Mismatch> mismatches)
+    {
+        public DateTime MismatchTime { get; set; } = mismatchTime;
+        public string SendLogMessage { get; set; } = sendLogMessage;
+        public string ResponseLogMessage { get; set; } = responseLogMessage;
+        public List<Mismatch> Mismatches { get; set; } = mismatches;
+    }
+}
